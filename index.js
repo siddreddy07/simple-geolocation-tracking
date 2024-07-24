e01211c2ece76275c2587b6ff447b5f0517bfcc2
siddreddy07 <siddharthreddy627@gmail.com>
2024-07-25 01:46:16 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const http = require("http");
+const path = require('path');
+const socketio = require('socket.io');
+
+
+const server = http.createServer(app);
+const io = socketio(server)
+
+app.set("view engine","ejs");
+app.use(express.static(path.join(__dirname,"public")));
+app.use(express.json())
+
+io.on("connection", function(socket){
+    socket.on("send-location",function(data){
+        io.emit("recieve-location",{id:socket.id , ...data});
+    });
+    
+    socket.on("disconnected",function(){
+        io.emit("user-disconnected",socket.id)
+    })
+})
+
+
+app.get("/",(req,res)=>{
+    res.render("index");
+})
+
+server.listen(7000,
+    console.log("Running")
+);
